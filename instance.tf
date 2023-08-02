e4d588c367077a16ac989d4ea5e00d27fc9f341f
Ranjini Ganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2023-08-02 21:51:02 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,75 @@
+data "aws_ami" "logstast-ami" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20230628.0-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "logstash-sg" {
+  name        = "logstash-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.logstast-ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.logstash-sg.id]
+  user_data = "${file("install_logstash.sh")}"
+
+  tags = {
+    Name = "logstash_instance"
+    owner = "ranjiniganeshan@gmail.com"
+  }
+}
+
+
